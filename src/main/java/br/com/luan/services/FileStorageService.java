6a0ca461be3d15bd27ead34731e95509bbdc8fd6
Luan Pinheiro <lluanps@gmail.com>
2023-03-11 21:19:50 -0300

    Preparando o Serviço para Prover Downloads

--- src/main/java/br/com/luan/services/FileStorageService.java
+++ src/main/java/br/com/luan/services/FileStorageService.java
@@ -6,12 +6,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.luan.config.FileStorageConfig;
 import br.com.luan.exceptions.FileStorageException;
+import br.com.luan.exceptions.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -45,5 +48,16 @@ public class FileStorageService {
 			throw new FileStorageException("Could not store file " + fileName + ". Please try again", e);
 		}
 	}
-
+	
+	public Resource loadFileAsResource(String filename) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) return resource;
+			else  throw new MyFileNotFoundException("File not found");
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found " + filename, e);
+		}
+	}
+	
 }
